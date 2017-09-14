0a6e1b544b3ac4bdd0533aa8f69d446861abc511
Conrad Davis Jr <conraddavis@google.com>
2017-09-14 16:55:57 -0700

    build a character builder

--- src/cube-animation/index.js
+++ src/cube-animation/index.js
@@ -10,7 +10,11 @@ import mountainSvg from './svgAssets/mountainSvg';
 // dom elements
 var environment = document.getElementById('environment');
 var camera = document.getElementById('camera');
+var characterBuilder = document.querySelector('.characterBuilder');
+var characterPreview = document.querySelector('.characterPreview');
 var character = document.querySelector('.character');
+var characterColorOption = document.querySelector('.characterBuilder .optionsContainer');
+var characterSelect = document.querySelector('.character-select');
 var mountainContainer = document.querySelector('.mountainContainer');
 var comet = document.querySelector('.comet');
 var alertIcon = document.querySelector('.alertIcon');
@@ -37,7 +41,15 @@ var controller = new ScrollMagic.Controller();
 // PROLOGUE
 // 
 
-// build tween1
+// character builder
+characterColorOption.addEventListener('click', (e) => {
+	characterPreview.style.backgroundColor = e.target.style.backgroundColor
+	character.style.backgroundColor = e.target.style.backgroundColor
+});
+
+characterSelect.addEventListener('click', (e) => {
+	characterBuilder.style.display = 'none';
+});
 // var tween1 = new TimelineMax();
 // tween1.to(character, 1, { transform:"translateX(calc(50vw))" })
 // tween1.to(mountain, 1, { x:'-100vw' }, "-=1");
@@ -77,8 +89,6 @@ tween2.to(groundImpact, 0, { opacity: 1}, "-=0.5")
 tween2.to(alertIcon, 0.1, {opacity: 0}, "-=0.2" );
 tween2.to(character, 1, { transform:"translateX(72vw) scale(0.8) translateY(500px) rotate(-190deg)" }, "-=0.5")
 tween2.to(environment, 1, { transform:"translateY(-80vh)" }, "-=1");
-// tween2.to(character, 1, { transform:"translateX(60vw) translateY(500px) rotate(-280deg)" }, "-=0.5");
-
 
 var scene2 = new ScrollMagic.Scene({ triggerElement: "#trigger2" })
   .setTween(tween2) // trigger a TweenMax.to tween
@@ -93,8 +103,6 @@ var scene2 = new ScrollMagic.Scene({ triggerElement: "#trigger2" })
 // build tween3
 var tween3 = new TimelineMax();
 tween3.to(character, 1, { transform:"translateX(32vw) scale(1) translateY(80vh) rotate(-230deg)" });
-// tween3.to(character, 1, { transform:"translateX(60vw) translateY(500px) rotate(-280deg)" }, "-=0.5");
-
 
 var scene3 = new ScrollMagic.Scene({ triggerElement: "#trigger3", duration: 1000 })
   .setTween(tween3) // trigger a TweenMax.to tween
