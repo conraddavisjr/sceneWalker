bdeb8de96c70f06dcaa1c7e1b23801ed619eb753
Conrad Davis Jr <conraddavis@google.com>
2017-09-14 14:25:14 -0700

    built scene 3

--- src/cube-animation/index.js
+++ src/cube-animation/index.js
@@ -12,6 +12,9 @@ var environment = document.getElementById('environment');
 var camera = document.getElementById('camera');
 var character = document.querySelector('.character');
 var mountainContainer = document.querySelector('.mountainContainer');
+var comet = document.querySelector('.comet');
+var alertIcon = document.querySelector('.alertIcon');
+var groundImpact = document.querySelector('.groundImpact');
 
 // populate the scene
 mountainContainer.innerHTML = mountainSvg;
@@ -30,6 +33,20 @@ var mountain = document.querySelector('.mountain');
 // init ScrollMagic controller
 var controller = new ScrollMagic.Controller();
 
+// 
+// PROLOGUE
+// 
+
+// build tween1
+// var tween1 = new TimelineMax();
+// tween1.to(character, 1, { transform:"translateX(calc(50vw))" })
+// tween1.to(mountain, 1, { x:'-100vw' }, "-=1");
+
+
+// var scene1 = new ScrollMagic.Scene({ triggerElement: "#trigger1", duration: 1000 })
+//   .setTween(tween1) // trigger a TweenMax.to tween
+//   .addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
+  // .addTo(controller);
 
 // 
 // SCENE 1
@@ -37,11 +54,11 @@ var controller = new ScrollMagic.Controller();
 
 // build tween1
 var tween1 = new TimelineMax();
-tween1.to(character, 1, { transform:"translateX(calc(10vw))" })
+tween1.to(character, 1, { transform:"translateX(calc(50vw))" })
 tween1.to(mountain, 1, { x:'-100vw' }, "-=1");
 
 
-var scene1 = new ScrollMagic.Scene({ triggerElement: "#trigger1", duration: 2000 })
+var scene1 = new ScrollMagic.Scene({ triggerElement: "#trigger1", duration: 1000 })
   .setTween(tween1) // trigger a TweenMax.to tween
   .addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
   .addTo(controller);
@@ -53,22 +70,35 @@ var scene1 = new ScrollMagic.Scene({ triggerElement: "#trigger1", duration: 2000
 // 
 
 // build tween2
-// var tween2 = new TimelineMax();
-// tween2.to(cube, 0.5, { transform:"rotateX(180deg) rotateY(100deg)" })
-// tween2.to(camera, 0.5, { transform:"scale(1) rotateY(0deg) translate(calc(50% - 200px), 20%)" }, "-=0.5")
-// tween2.to(cube, 0.5, { transform:"rotateX(180deg) rotateY(90deg)" }, "-=0.3")
-// tween2.to(topFace, 0.5, { transform:"rotateX(120deg) translateZ(490px) translateY(-415px) translateX(-100px)" }, "-=0.5")
-// tween2.to(rightFace, 0.5, { transform:"rotateY(90deg) rotateX(-40deg) translateZ(-340px) translateY(-220px)" }, "-=0.5")
-// tween2.to(bottomFace, 0.5, { transform:"rotateX(-150deg) rotateY(77deg) translateZ(680px) translateY(100px)" }, "-=0.5")
-// tween2.to(leftFace, 0.5, { transform:"rotateY(-20deg) rotateX(30deg) translateZ(530px)" }, "-=0.5")
-// tween2.to(frontFace, 0.5, { transform:"rotateY(50deg) translateZ(550px) translateY(-40px)" }, "-=0.5") 
-// tween2.to(backFace, 0.5, { transform:"rotateY(170deg) rotateX(-20deg) translateZ(480px) translateY(-221px)" }, "-=0.5");
-
-// tween2.to(topFace, 1, { transform:"rotateX(120deg) translateZ(100px) translateY(-175px)" }, "-=1")
-// tween2.to(environment, 1, { backgroundColor: 'white' }, "-=1");
+var tween2 = new TimelineMax();
+tween2.to(alertIcon, 0.1, {opacity: 1} )
+tween2.to(comet, 1, { transform:"translateX(60vw) translateY(400px)" }, "+=0.3")
+tween2.to(groundImpact, 0, { opacity: 1}, "-=0.5")
+tween2.to(alertIcon, 0.1, {opacity: 0}, "-=0.2" );
+tween2.to(character, 1, { transform:"translateX(72vw) scale(0.8) translateY(500px) rotate(-190deg)" }, "-=0.5")
+tween2.to(environment, 1, { transform:"translateY(-80vh)" }, "-=1");
+// tween2.to(character, 1, { transform:"translateX(60vw) translateY(500px) rotate(-280deg)" }, "-=0.5");
 
 
-// var scene2 = new ScrollMagic.Scene({ triggerElement: "#trigger2" })
-//   .setTween(tween2) // trigger a TweenMax.to tween
-//   .addIndicators({name: "2 (duration: 0)"}) // add indicators (requires plugin)
-//   .addTo(controller);
+var scene2 = new ScrollMagic.Scene({ triggerElement: "#trigger2" })
+  .setTween(tween2) // trigger a TweenMax.to tween
+  .addIndicators({name: "2 (duration: 0)"}) // add indicators (requires plugin)
+  .addTo(controller);
+
+
+// 
+// SCENE 3
+// 
+
+// build tween3
+var tween3 = new TimelineMax();
+tween3.to(character, 1, { transform:"translateX(32vw) scale(1) translateY(80vh) rotate(-230deg)" });
+// tween3.to(character, 1, { transform:"translateX(60vw) translateY(500px) rotate(-280deg)" }, "-=0.5");
+
+
+var scene3 = new ScrollMagic.Scene({ triggerElement: "#trigger3", duration: 1000 })
+  .setTween(tween3) // trigger a TweenMax.to tween
+  .addIndicators({name: "3 (duration: 0)"}) // add indicators (requires plugin)
+  .addTo(controller);
+
+
